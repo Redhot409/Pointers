2d4b675d410287fe4bcf0013d66c2f46c377178e
Alexander Melikhov <alexandersermel@gmail.com>
2024-04-22 19:47:00 +0300

    Pointers created

---
+++ Pointers/main.cpp
@@ -0,0 +1,15 @@
+//Pointers
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl;// адрес в оперативной памяти
+
+
+
+}
